cee3c965b7a5d9ec0d1eb8cf78b821bc32835d03
ruchisharmaku-debug <ruchisharmaku@gmail.com>
2026-01-30 20:14:53 +0530

    Add files via upload

---
+++ 10.py
@@ -0,0 +1,34 @@
+# Strings are immutable
+a = "!!!Ruchi!! !!!!!!!!! Ruchi"
+print(len(a))
+print(a)
+print(a.upper())
+print(a.lower())
+print(a.rstrip("!"))
+print(a.replace("Ruchi","john"))
+print(a.split(" "))
+blogHeading= "introduction to js"
+print(blogHeading.capitalize())
+
+str = "Welcome to the Console!!!"
+print(len(str1))
+print(len(str1.center(50)))
+
+print(a.count("Ruchi"))
+str1="Welcome to console !!!"
+print(str1.endswith("!!!"))
+
+str1= "Welcome to the console !!!"
+print(str1.endswith("to",4,10))
+
+str1= "He's name is Dan. He is an honest man."
+print(str1.find("is"))
+
+str1= "Python is a Interpreted Language"
+print(str1.startswith("Python"))
+
+str1= "Python is a Interpreted Language"
+print(str1.swapcase())
+
+str1= "He's name is Dan. He is an honest man."
+print(str1.tittle())
