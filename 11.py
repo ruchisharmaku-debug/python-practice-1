4a82d1a23e8a295c1463e9a3ff3acae7bfc11396
ruchisharmaku-debug <ruchisharmaku@gmail.com>
2026-01-31 19:27:35 +0530

    Add files via upload

---
+++ 11.py
@@ -0,0 +1,12 @@
+a = int(input("Enter your age:"))
+print("Your age is :", a)
+# Conditional operators
+# >, <, >=, <= ,==,!=
+print(a>18)
+print(a<=18)
+print(a==18)
+print(a!=18)
+if (a>18):
+    print("you can drive")
+else:
+    print("you cannot drive")
---
+++ if-else.py
@@ -0,0 +1,9 @@
+applePrice = 10
+budget = 200 
+if (budget - applePrice > 50):
+    print("Alexa, add 1 kg Apples to the cart.")
+
+elif (budget - applePrice >70):
+    print("Its okay you can buy")
+else:
+    print("Alexa, do not add Apples to the cart.")
