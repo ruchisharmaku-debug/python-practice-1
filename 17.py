b2b35e04ca850d6ca519182b653ce52b5a31a7fd
ruchisharmaku-debug <ruchisharmaku@gmail.com>
2026-02-04 20:05:16 +0530

    loop
    break → loop ko completely exit karta hai
    continue → current iteration skip karta hai
    Loop control statements code ko efficient & readable banate hain

---
+++ 17.py
@@ -0,0 +1,7 @@
+for i in range(12):
+    if(i == 10):
+        break
+    print("5 x", i+1 , "=", 5 *(i+1))
+
+
+print("Loop ko chodkar nikal gyi")
---
+++ 18.py
@@ -0,0 +1,6 @@
+for i in range(12):
+    if(i == 10):
+        print("Skip the iteration")
+        continue
+    print("5 x", i, "=", 5*i)
+    
